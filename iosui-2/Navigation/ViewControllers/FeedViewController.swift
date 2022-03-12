667b10068589850825df8f4c2fe74258954f9959
FuLLhaySs <fullhays@icloud.com>
2022-03-12 15:44:36 +0300

    iosui2

---
+++ iosui-2/Navigation/ViewControllers/FeedViewController.swift
@@ -0,0 +1,36 @@
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+    var postButton: UIButton!
+    
+    var post = Post()
+    
+    override func loadView() {
+        view = UIView()
+        
+        postButton = UIButton()
+        postButton.backgroundColor = .systemCyan
+        postButton.layer.borderWidth = 1
+        postButton.layer.cornerRadius = 5
+        postButton.frame = CGRect(x: 20, y: 200, width: 100, height: 50)
+        postButton.setTitle("Пост", for: .normal)
+        postButton.addTarget(self, action: #selector(showPost), for: .touchUpInside)
+        view.addSubview(postButton)
+        
+
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @objc func showPost() {
+        let postVC = PostViewController()
+        postVC.post = post
+        self.navigationController?.pushViewController(postVC, animated: true)
+    }
+
+}
---
+++ iosui-2/Navigation/ViewControllers/PostViewController.swift
@@ -0,0 +1,26 @@
+
+import UIKit
+
+class PostViewController: UIViewController {
+    
+    var post: Post!
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .systemCyan
+
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = post.title
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Info", style: .done, target: self, action: #selector(showInfo))
+    }
+    
+    @objc private func showInfo() {
+        let infoVC = InfoViewController()
+        self.present(infoVC, animated: true)
+    }
+}
